f8a2ab0936d5d1ac79dabf515e0891f7a4505037
bilalboultif <80066430+bilalboultif@users.noreply.github.com>
2023-03-05 00:53:22 -0500

    add images

---
+++ src/media/index.js
@@ -0,0 +1,82 @@
+import Radio from '../media/Radio.jpg';
+import CCTV from '../media/CCTV-Camera.jpg';
+import CLOCK from '../media/Clock-In.jpg';
+import COWBattery from '../media/COW-Battery.jpg';
+import COWInvertor from '../media/COW-Invertor.jpg';
+import Deskradio from '../media/Desk-radio.jpg';
+import Desktop from '../media/Desktop.jpg';
+import DockingStation from '../media/Docking-Station.jpg';
+import FasternalMachine from '../media/Fasternal-Machine.jpg';
+import HPPrinter from '../media/HP-Printer.jpg';
+import HPThinClient from '../media/HP-ThinClient.jpg';
+import iPhone from '../media/Mobile.jpg';
+import Monitor from '../media/PC-Screen.jpg';
+import Projector from '../media/Projector.jpg';
+import MC33Scanner from '../media/RF-Scanner.jpg';
+import Tablet from '../media/Tablet.jpg';
+import TC56 from '../media/TC56.jpg';
+import TVScreen from '../media/TV-Screen.jpg';
+import USBScanner from '../media/usbScanner.jpg';
+import Yubikey from '../media/Yubikey.jpg';
+import ZebraCardPrinter from '../media/Zebra-Card-Printer.jpg';
+import ZebraPrinter from '../media/Zebra-Printer.jpg';
+import ZebraMobilePrinter from '../media/Zebra-Mobile-Printer.jpg';
+import ZebraSlamPrinter from '../media/Zebra-SLAM-Printer.jpg';
+import Laptop from '../media/Laptop.jpg';
+import COW from '../media/COW.jpg';
+import BluetoothScanner from '../media/1991i-scanner.jpg';
+import WATIT from '../media/WAT-IT.jpg';
+import Account from '../media/Account.jpg';
+import ALPR from '../media/ALPR.jpg';
+import Genovation from '../media/Genovation.jpg';
+import SpooScanner from '../media/spooScanner.png';
+import FastRiver from '../media/fastRiverLogo.png';
+import AccoladeIco from '../media/accolade.png';
+import Pathfinder from '../media/Pathfinder.jpg';
+import RAD from '../media/rad.png';
+import Amazon from '../media/amazon.png';
+import Inbound from '../media/inbound.png';
+
+const Picture = {
+    Radio,
+    CCTV,
+    CLOCK,
+    COWBattery,
+    COWInvertor,
+    Deskradio,
+    Desktop,
+    DockingStation,
+    FasternalMachine,
+    HPPrinter,
+    HPThinClient,
+    iPhone,
+    Monitor,
+    Projector,
+    MC33Scanner,
+    Tablet,
+    TC56,
+    TVScreen,
+    USBScanner,
+    Yubikey,
+    ZebraCardPrinter,
+    ZebraPrinter,
+    ZebraMobilePrinter,
+    ZebraSlamPrinter,
+    Laptop,
+    COW,
+    BluetoothScanner,
+    WATIT,
+    Account,
+    ALPR,
+    Genovation,
+    SpooScanner,
+    FastRiver,
+    AccoladeIco,
+    Pathfinder,
+    RAD,
+    Amazon,
+    Inbound
+
+};
+
+export default Picture;
