e23780281c0f06eabaf4e5f51f47d16ea4075bec
bilalboultif <80066430+bilalboultif@users.noreply.github.com>
2023-03-12 04:01:38 -0400

    add css dot

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavigationDots = ({ active }) => (
   <div className="app__navigation">
-    {['home', 'about', 'work', 'skills', 'testimonial', 'IT','login'].map((item, index) => (
+    {['home', 'about','outbound',  'inbound', 'IT','login'].map((item, index) => (
       <a
         href={`#${item}`}
         key={item + index}
